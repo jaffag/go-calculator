a09e02f9d37f9ff5cb788bc6f3f277f77e4d2d0b
jaffag <jafarabdulg@gmail.com>
2025-02-15 13:01:31 +0700

    [new&update] rectangle & circle

--- src/circle/circle.go
+++ src/circle/circle.go
@@ -2,10 +2,10 @@ package circle
 
 import "math"
 
-func GetCircleCircumference(radius float64) float64 {
+func GetCircumference(radius float64) float64 {
 	return 2 * math.Phi * radius
 }
 
-func GetCircleArea(radius float64) float64 {
+func GetArea(radius float64) float64 {
 	return math.Phi * math.Pow(radius, 2)
 }
---
+++ src/rectangle/rectangle.go
@@ -0,0 +1,9 @@
+package rectangle
+
+func GetCircumference(l float64, w float64) float64 {
+	return 2 * (l + w)
+}
+
+func GeArea(l float64, w float64) float64 {
+	return l * w
+}
